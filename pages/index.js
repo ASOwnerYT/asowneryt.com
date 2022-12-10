3766a51cae937a1a7b663956d73d6f7152b4cd60
ASOwnerYT <asowner@outlook.com>
2022-12-10 21:37:12 +1300

    Display posts data on home and path

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,45 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Greetings! I'm ASOwnerYT</p>
-        <p>
-          i will add more description latr
-        </p>
+      <section className="text-xl">
+        <p className="font-bold">Greetings! I'm ASOwnerYT</p>
+        <ul className="list-disc list-inside">
+          <li>HTML, CSS, JS</li>
+          <li>React and NextJS</li>
+          <li>Video editing</li>
+        </ul>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
